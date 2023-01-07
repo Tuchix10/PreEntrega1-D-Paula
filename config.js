9e85d7e8f8294bd38e3863a650a7943033dd492a
Tuchix10 <tuchix10@gmail.com>
2023-01-07 19:40:08 -0300

    Agregando For y corrigiendo Css

--- config.js
+++ config.js
@@ -1,44 +1,78 @@
-function calculo () {
-    let monto=Number(document.getElementById("dinero").value)
-    let tm=(75/12)/100
-    if(monto>=1000 && monto<=10000000) {
-        if (document.getElementById("radio1").checked) {
-            d1=monto*tm;
-            r1=d1+monto;
-            document.getElementById("resultado").innerHTML=new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(r1);
+const minimoPermitido = 1000;
+const maximoPermitido = 10000000;
+const periodoMaximo = 60;
+const periodoMinimo = 1;
+const tasa=75;
+const tasaMensual=(tasa/12)/100;
+
+function reinvertir() {
+    return document.getElementById("reinvertirSi").checked;
+}
+
+function calcularPorPeriodo(monto) {
+    if (reinvertir()) {
+        const d4 = monto * (1 + tasaMensual) ** periodo();
+        return d4.toFixed(2);
+    }
+    const tasaPeriodo = tasaMensual * periodo();
+    return monto * (1 + tasaPeriodo);
+}
+
+function validarMonto(monto) {
+    if (monto < minimoPermitido) {
+        alert(`El minimo permitido para un plazo fijo es ${minimoPermitido}`);
+        return false;
+    }
+    if (monto > maximoPermitido) {
+        alert(`El maximo permitido para un plazo fijo es ${maximoPermitido}`);
+        return false;
+    }
+    return true;
+}
+
+function validarPeriodo(periodo) {
+    if (periodo < periodoMinimo) {
+        alert(`El minimo permitido para un plazo fijo es ${periodoMinimo}`);
+        return false;
+    }
+    if (periodo > periodoMaximo) {
+        alert(`El maximo permitido para un plazo fijo es ${periodoMaximo}`);
+        return false;
+    }
+    return true;
+}
+
+function generarTabla () {
+    document.getElementById("tab").innerHTML="";
+    const monto = Number(document.getElementById("dinero").value);
+    const periodo = Number(document.getElementById("periodo").value);
+    if ((!validarMonto(monto)) || (!validarPeriodo(periodo))) {
+        return;
+    }
+    if (reinvertir()) {
+        for(i=1,k=monto;i<=periodo;i++,k=k+resultado){
+            resultado=k*tasaMensual
+            dato1=resultado.toFixed(2);
+            dato2=resultado+k;
+            dato3=dato2.toFixed(2)
+            document.getElementById("tab").innerHTML=document.getElementById("tab").innerHTML+
+                    `<tr>
+                        <td> ${i}</td>
+                        <td> ${k.toFixed(2)}</td>
+                        <td> ${dato1}</td>
+                        <td> ${dato3}</td>
+                    </tr>`;
         }
-        if (document.getElementById("radio2").checked) {
-            if (document.getElementById("radio5").checked) {
-                tt=tm*3;
-                d2=monto*tt;
-                r2=d2+monto;
-                document.getElementById("resultado").innerHTML=new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(r2);
-            }
-        }
-        if (document.getElementById("radio3").checked) {
-            if (document.getElementById("radio5").checked) {
-                tna=75/100
-                d3=monto*tna;
-                r3=d3+monto;
-                document.getElementById("resultado").innerHTML=new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(r3);
-            }
-        }
-        if (document.getElementById("radio2").checked) {
-            if (document.getElementById("radio4").checked) {
-                d4=monto*(1+tm)**3;
-                r4=d4.toFixed(2);
-                document.getElementById("resultado").innerHTML=new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(r4);
-            }
-        }
-        if (document.getElementById("radio3").checked) {
-            if (document.getElementById("radio4").checked) {
-                d5=monto*(1+tm)**12
-                r5=d5.toFixed(2)
-                document.getElementById("resultado").innerHTML=new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(r5);
-            }
-        }
+        ganancia=dato3-monto;
+        document.getElementById("t1").innerHTML=monto;
+        document.getElementById("t2").innerHTML=ganancia;
+        document.getElementById("t3").innerHTML=dato3;
     }
     else {
-        alert ("El minimo permitido para un plazo fijo es $1000")
+        ganancia=(monto*(tasaMensual*periodo));
+        totalSinReinvertir=monto+ganancia;
+        document.getElementById("t1").innerHTML=monto;
+        document.getElementById("t2").innerHTML=ganancia;
+        document.getElementById("t3").innerHTML=totalSinReinvertir;
     }
 }
